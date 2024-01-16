8ebd97cb08f5aaffe9dc2f9cd01c877def16a880
UlvakSkillz <156770480+UlvakSkillz@users.noreply.github.com>
2024-01-15 23:00:05 -0800

    Initial commit

---
+++ MoreParkFriends/MoreParkFriendsClass.cs
@@ -0,0 +1,19 @@
+﻿using MelonLoader;
+using UnityEngine;
+using RUMBLE.Environment;
+using HarmonyLib;
+
+[HarmonyPatch(typeof(ParkBoardGymVariant), "OnPlayerEnteredTrigger")] // Example is the name of the class
+public static class Patch
+{
+    private static void Prefix()
+    {
+        ParkBoardGymVariant parkBoardGymVariant = GameObject.Find("--------------LOGIC--------------/Heinhouser products/Parkboard").gameObject.GetComponent<ParkBoardGymVariant>();
+        parkBoardGymVariant.hostPlayerCapacity *= 2;
+    }
+}
+
+namespace MoreParkFriends
+{
+    public class MoreParkFriendsClass : MelonMod {}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MoreParkFriends
+ Sets the Park Count to x2 Selected Value
