d8e2c7f898656d367c464aaebd9c1378c31aaa54
UlvakSkillz <156770480+UlvakSkillz@users.noreply.github.com>
2024-01-16 23:47:21 -0800

    .

--- MoreParkFriends/MoreParkFriendsClass.cs
+++ MoreParkFriends/MoreParkFriendsClass.cs
@@ -2,18 +2,45 @@
 using UnityEngine;
 using RUMBLE.Environment;
 using HarmonyLib;
+using MoreParkFriends;
 
 [HarmonyPatch(typeof(ParkBoardGymVariant), "OnPlayerEnteredTrigger")]
 public static class Patch
 {
+
     private static void Prefix()
     {
+        int multiplier = MoreParkFriendsClass.multiplier;
         ParkBoardGymVariant parkBoardGymVariant = GameObject.Find("--------------LOGIC--------------/Heinhouser products/Parkboard").gameObject.GetComponent<ParkBoardGymVariant>();
-        parkBoardGymVariant.hostPlayerCapacity *= 2;
+        parkBoardGymVariant.hostPlayerCapacity *= multiplier;
     }
 }
 
 namespace MoreParkFriends
 {
-    public class MoreParkFriendsClass : MelonMod {}
+    public class MoreParkFriendsClass : MelonMod 
+    {
+        public static int multiplier = int.Parse(System.IO.File.ReadAllText(@"UserData\MoreParkFriends\Multiplier.txt"));
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            base.OnSceneWasLoaded(buildIndex, sceneName);
+            if (sceneName == "Gym")
+            {
+                if (multiplier <= 0)
+                {
+                    multiplier = 1;
+                }
+                if (9 < multiplier)
+                {
+                    multiplier = 9;
+                }
+                GameObject gameObject = GameObject.Find("--------------LOGIC--------------/Heinhouser products/Parkboard/RotatingScreen/HostPanel/Player Cpapcity/TextandIcons/");
+                for (int i = 1; i <= 5; i++)
+                {
+                    gameObject.transform.GetChild(i).GetComponent<TMPro.TextMeshPro>().text = ((1+i) * multiplier).ToString();
+                }
+            }
+        }
+    }
 }
